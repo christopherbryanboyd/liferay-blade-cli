7c6d83da0e0591f8882dd53370a763cb70745e4b
Christopher Bryan Boyd <christopher.boyd@liferay.com>
2019-05-08 14:06:10 -0500

    BLADE-162 Implement CommandHistoryDto hashcode and equals

--- cli/src/main/java/com/liferay/blade/cli/util/CommandHistoryDto.java
+++ cli/src/main/java/com/liferay/blade/cli/util/CommandHistoryDto.java
@@ -83,6 +83,57 @@ public class CommandHistoryDto {
 	}
 
 	private String args;
+	@Override
+	public int hashCode() {
+		final int prime = 31;
+		int result = 1;
+		result = prime * result + ((args == null) ? 0 : args.hashCode());
+		result = prime * result + ((exceptionString == null) ? 0 : exceptionString.hashCode());
+		result = prime * result + exitCode;
+		result = prime * result + ((name == null) ? 0 : name.hashCode());
+		result = prime * result + ((profile == null) ? 0 : profile.hashCode());
+		result = prime * result + (int) (timeInvoked ^ (timeInvoked >>> 32));
+		return result;
+	}
+
+
+
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj)
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		CommandHistoryDto other = (CommandHistoryDto) obj;
+		if (args == null) {
+			if (other.args != null)
+				return false;
+		} else if (!args.equals(other.args))
+			return false;
+		if (exceptionString == null) {
+			if (other.exceptionString != null)
+				return false;
+		} else if (!exceptionString.equals(other.exceptionString))
+			return false;
+		if (exitCode != other.exitCode)
+			return false;
+		if (name == null) {
+			if (other.name != null)
+				return false;
+		} else if (!name.equals(other.name))
+			return false;
+		if (profile == null) {
+			if (other.profile != null)
+				return false;
+		} else if (!profile.equals(other.profile))
+			return false;
+		if (timeInvoked != other.timeInvoked)
+			return false;
+		return true;
+	}
+
 	private String exceptionString;
 	private int exitCode;
 	private String name;
